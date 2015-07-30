af255313d8f66b579a2d7f9367b1993480e15f8a
yang <yang@ubuntu.(none)>
2015-07-30 17:14:30 +0800

    del the hbase request of baiduspyder

--- spiders/spiders/spiders/baiduNews_spider.py
+++ spiders/spiders/spiders/baiduNews_spider.py
@@ -39,11 +39,11 @@ class BaiduNewSpider(Spider):
         self.log('---started----')
         self.getStartUrl()
         self.r = Redis(host = self.tool.HOST_REDIS, port = 6379, db = 0)
-        self.htable=HBaseTest(table = 'origin')
+        #self.htable=HBaseTest(table = 'origin')
 
     def finalize(self):
         self.log('---stopped---')
-        self.htable.close_trans()
+        #self.htable.close_trans()
         #url持久化
 
     def getStartUrl(self):
@@ -84,8 +84,8 @@ class BaiduNewSpider(Spider):
         if item['url'].find('?') >= 0:
             item['url'] = response.url
             if self.r.sismember('crawled_set', item['url']):
-                if self.htable.getRowByColumns(item['url'], ['indexData:url']):
-                    return                         
+                #if self.htable.getRowByColumns(item['url'], ['indexData:url']):
+                return                         
         
         if response.body:
             bsoup = BeautifulSoup(response.body,from_encoding='utf-8')
@@ -136,8 +136,8 @@ class BaiduNewSpider(Spider):
                     continue
 
                 if self.r.sismember('crawled_set', item['url']):
-                    if self.htable.getRowByColumns(item['url'], ['indexData:url']):
-                        continue
+                    #if self.htable.getRowByColumns(item['url'], ['indexData:url']):
+                    continue
             
                 item['collecttime'] = time.strftime("%Y-%m-%d %H:%M", time.localtime())
                 if elem.find('div',class_='c-summary'):
